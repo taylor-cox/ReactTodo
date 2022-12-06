e364fcf13b99042aebc12c5b760e5c1cc7f92b2e
taylor-cox <taylor@coxintl.com>
2022-12-06 12:32:36 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoHeader>
+
+      </TodoHeader>
+      <TodoInput>
+
+      </TodoInput>
+      <TodoItem>
+
+      </TodoItem>
+    </div>
+  );
+}
+
+export default App;
